9d14e37ece3ddc8f510fccd33392138293c66baf
Ali Bahari <alibahari007@gmail.com>
2022-10-19 16:06:06 +0330

    Add initial text chat to frontend

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,12 +1,33 @@
 <template>
     <div>
-        Welcome to ComeOnMan Frontend!
+        <input @input="onInput" @keyup.enter="onSubmit"/>
+        <button @click="onSubmit"> Send! </button>
+        <li v-for="{msg, time} in this.chat" :key="time">
+            {{msg}} - {{time}}
+        </li>
     </div>
 </template>
 
 <script>
 export default {
-    name: "App"
+    name: "App",
+    data: () => {
+        return {
+            message: "",
+            chat: []
+        }
+    },
+    methods: {
+        onInput(event) {
+            this.message = event.target.value
+        },
+        onSubmit() {
+            this.chat.push({
+                msg: this.message,
+                time: new Date().toLocaleTimeString()
+            })
+        }
+    }
 }
 </script>
 

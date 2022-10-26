6a519e67a2a538326cf1860c2d1d414d4d276df3
Ali Bahari <alibahari007@gmail.com>
2022-10-26 16:48:47 +0330

    Add greeting message in chat view

--- front-end/src/components/ChatView.vue
+++ front-end/src/components/ChatView.vue
@@ -1,32 +1,37 @@
 <template>
-    <TransitionGroup name="slide" :class="[isChat ? 'on-middle' : '']">
-        <div 
-            v-for="{user, content, time} in chat"
-            :key="time + user.name"
-            :class="getMessageClassName(user.socketID)"
-        >
-            <div class="message" v-if="user.socketID === $socket.id" >
-                <div style="display: flex; flex-direction: column; flex: 1; justify-content: space-between ; margin-right: 20px;">
-                    <span style="margin: 10px; word-break: break-word;">{{content}}</span>
-                    <span style="font-size: 10px; margin-bottom: 5px;">{{time}}</span>
-                </div>
-                <div style="display: flex; flex-direction: column; align-items: center;">
-                    <img :src="user.avatar"/>
-                    <span class="username">{{user.name}}</span>
-                </div>
-            </div>
-            <div class="message" v-else>
-                <div style="display: flex; flex-direction: column; align-items: center;">
-                    <img :src="user.avatar"/>
-                    <span class="username">{{user.name}}</span>
-                </div>                
-                <div style="display: flex; flex-direction: column; flex: 1; justify-content: space-between ; margin-left: 10px;">
-                    <span style="margin: 10px; word-break: break-word;">{{content}}</span>
-                    <span style="font-size: 10px; margin-bottom: 5px; align-self: flex-end;">{{time}}</span>
-                </div>               
-            </div>   
+    <div :class="[isChat ? 'on-middle' : '']" >
+        <div v-if="!chat.length" :class="[isChat ? 'greeting' : '']">
+            Start Chatting Now...
         </div>
-    </TransitionGroup>
+        <TransitionGroup v-else name="slide" :class="[isChat ? 'chat-wrapper' : '']">
+            <div 
+                v-for="{user, content, time} in chat"
+                :key="time + user.name"
+                :class="getMessageClassName(user.socketID)"
+            >
+                <div class="message" v-if="user.socketID === $socket.id" >
+                    <div style="display: flex; flex-direction: column; flex: 1; justify-content: space-between ; margin-right: 20px;">
+                        <span style="margin: 10px; word-break: break-word; white-space: pre-wrap;">{{content}}</span>
+                        <span style="font-size: 10px; margin-bottom: 5px;">{{time}}</span>
+                    </div>
+                    <div style="display: flex; flex-direction: column; align-items: center;">
+                        <img :src="user.avatar"/>
+                        <span class="username">{{user.name}}</span>
+                    </div>
+                </div>
+                <div class="message" v-else>
+                    <div style="display: flex; flex-direction: column; align-items: center;">
+                        <img :src="user.avatar"/>
+                        <span class="username">{{user.name}}</span>
+                    </div>                
+                    <div style="display: flex; flex-direction: column; flex: 1; justify-content: space-between ; margin-left: 10px;">
+                        <span style="margin: 10px; word-break: break-word; white-space: pre-wrap;">{{content}}</span>
+                        <span style="font-size: 10px; margin-bottom: 5px; align-self: flex-end;">{{time}}</span>
+                    </div>               
+                </div>   
+            </div>
+        </TransitionGroup>
+    </div>
 </template>
 
 <script>
@@ -59,13 +64,38 @@ img {
     margin-right: 10px;
     border-radius: 50%;
 }
+
 .on-middle {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    flex: 1;
+    overflow-y: scroll;
+    scrollbar-width: none;
+    animation: expandMiddle 4s forwards;
+}
+
+.chat-wrapper {
     display: flex;
     flex-direction: column-reverse;
     flex: 1;
     overflow-y: scroll;
     scrollbar-width: none;
-    animation: expandMiddle 4s forwards;
+    animation: expandChat 1.5s forwards;
+}
+
+.greeting {
+    font-family: 'SF Pro Display', sans-serif;
+    font-weight: bolder;    
+    color: white;
+    font-size: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    overflow-y: scroll;
+    scrollbar-width: none;
+    animation: expandMiddleGreeting 5.5s forwards;
 }
 
 @keyframes expandMiddle{
@@ -76,6 +106,29 @@ img {
         flex: 1;
     }
 }
+
+@keyframes expandChat{
+    0% {
+        flex: 0;
+        width: 80%;
+    }
+    100% {
+        flex: 1;
+        width: 100%;
+    }
+}
+
+@keyframes expandMiddleGreeting{
+    0% {
+        flex: 0;
+        opacity: 0;
+        transform: scale(0.2);
+    }
+    100% {
+        flex: 1;
+    }
+}
+
 .own-wrapper {
     max-width: 65%;
     align-self: flex-end;
@@ -105,12 +158,12 @@ img {
 .username {
     font-family: 'SF Pro Display', sans-serif;
     font-weight: bolder;
-    font-size: 12px;
+    font-size: 11px;
     margin-bottom: 10px;
     margin-left: 10px;
     margin-right: 10px;
     word-break: break-word;
-    max-width: 50px;
+    max-width: 85px;
     text-align: center;
 }
 
@@ -130,7 +183,7 @@ img {
         transform: translateY(0px);
     }
     80% {
-        opacity: 0.8;
+        opacity: 0.6;
         transform: translateY(5px);
     }    
 }
